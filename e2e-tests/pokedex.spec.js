ec4ee4b493fc73b580a43af6017bcaf314764225
Franco Redivo <franco161101@gmail.com>
2026-04-14 12:58:40 -0300

    fix: update import statement and format test cases in pokedex.spec.js

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -1,9 +1,15 @@
-const { test, describe, expect, beforeEach } = require('@playwright/test')
+import { test, describe, expect } from '@playwright/test'
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
     await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
-  })
+  });
+
+  test('pokemon details can be opened', async ({ page}) => {
+    await page.goto('')
+    await page.getByRole('link', { name: 'ivysaur' }).click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  });
 })
