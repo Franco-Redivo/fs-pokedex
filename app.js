7d8d4cc15471287ddd6551a290f1f9614d93b557
Franco Redivo <franco161101@gmail.com>
2026-04-14 15:21:48 -0300

    fix: add version endpoint to return current application version

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.static('dist'))
 const start = async () => {
   await app.listen(PORT)
   console.log(`server started on port ${PORT}`)
+  app.get('/version', (req, res) => {
+    res.send('1') // change this string to ensure a new version deployed
+  })
 }
 
 start()
