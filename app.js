deeadbbd95f21f1c81ff9931cd72596fb6020d74
Franco Redivo <franco161101@gmail.com>
2026-04-14 17:34:47 -0300

    feat: update deployment pipeline and version endpoint

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const start = async () => {
   await app.listen(PORT)
   console.log(`server started on port ${PORT}`)
   app.get('/version', (req, res) => {
-    res.send('1') // change this string to ensure a new version deployed
+    res.send('2') // change this string to ensure a new version deployed
   })
 }
 
