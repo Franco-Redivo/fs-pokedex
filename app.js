f059ce635c3c169af43e2eba0ae34e28c54bc5d8
Franco Redivo <franco161101@gmail.com>
2026-04-15 16:58:19 -0300

    feat: add health check endpoint to the application

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ app.use(express.static('dist'))
 const start = async () => {
   await app.listen(PORT)
   console.log(`server started on port ${PORT}`)
+
+  app.get('/health', (req, res) => {
+    res.send('ok')
+  })
+
   app.get('/version', (req, res) => {
     res.send('2') // change this string to ensure a new version deployed
   })
